ae1bed094d1aa60729c96bc5bbeb151de70578aa
Samuel de Oliveira <samueldeoliveira578@gmail.com>
2026-02-12 22:02:59 -0300

    Add DollarToBRL currency converter project

---
+++ 05-DollarToBRL/DollarToBRL/CurrencyConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace DollarToBRL
+{
+    internal class ConversorDeMoeda
+    {
+        static decimal IOF = 0.06m;
+        public static decimal Conversion(decimal exchangeRate, decimal amountInDollars)  {
+
+            return (exchangeRate * amountInDollars / (1.00m - IOF) );          
+        }
+    }
+}
---
+++ 05-DollarToBRL/DollarToBRL/Program.cs
@@ -0,0 +1,24 @@
+﻿using DollarToBRL;
+using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal exchangeRate = 0.0m;
+            decimal amountInDollars = 0.0m;
+
+            Console.Write("What is the exchange rate between the dollar and the real? ");
+            exchangeRate = decimal.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("\nHow many dollars do you want to convert to reais? USD ");
+            amountInDollars = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            decimal reais = ConversorDeMoeda.Conversion(exchangeRate, amountInDollars); 
+
+            Console.WriteLine("\nEstimated amount to be paid in reais (including IOF) = BRL " + reais.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
